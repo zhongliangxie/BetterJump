aa8280435b3378f64886fe50a6a1d43bb50f86ce
zhongliang xie <zhongliangxie@zhongliangdeMacBook-Pro.local>
2020-02-21 21:42:27 +0800

    first commit

---
+++ Assets/Scenes/Jump.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("jump");
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+}
